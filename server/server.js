82c7229a1941b581ac317dfd3ca0861777555645
KishorJJ11 <kishorjj05@gmail.com>
2025-12-28 18:53:13 +0530

    Deleted wrong files

--- server/server.js
+++ server/server.js
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  console.log(`Path: ${req.path} | Method: ${req.method}`);
+  next();
+});
+
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
